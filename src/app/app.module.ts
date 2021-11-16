c74ec1958124b27066439ec7a798fd61fd39d8b2
LDenis1 <limad2263gh@gmail.com>
2021-11-15 21:25:54 -0600

    Edit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ADDComponent } from './add/add.component';
+import { TableComponent } from './table/table.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ADDComponent,
+    TableComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
